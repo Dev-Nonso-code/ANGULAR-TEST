a9b51398cd77ac8a46b869427e6338093f12b733
Dev-Nonso-code <acreekane@gmail.com>
2024-07-21 14:12:35 +0100

    sunday commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,13 @@ import { FormsComponent } from './forms/forms.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 
+
+
 export const routes: Routes = [
 
     {path: "", pathMatch:"full", redirectTo:"app"},
     {path: "form", component:FormsComponent },
     {path: "dash", component:HomeComponent },
     {path: "nav", component:NavbarComponent },
+    
 ];
